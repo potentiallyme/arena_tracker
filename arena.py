679e22350099bb68e004f63e0b091be310b5abb1
Loïc Moran <112955036+potentiallyme@users.noreply.github.com>
2025-04-10 15:19:05 +0000

    added a search bar for qol

--- arena.py
+++ arena.py
@@ -18,13 +18,19 @@ def write_champs(champs):
         messagebox.showerror("Error", f"An error occurred while saving: {e}")
 
 def handle_entry():
+    search_name = search_entry.get().title()
     add_name = add_entry.get().title()
     replace_old = replace_entry.get().title()
     replace_new = to_entry.get().title()
     remove_name = remove_entry.get().title()
     champs = read_champs()
     
-    if add_name:
+    if search_name:
+        if search_name in champs:
+            messagebox.showinfo("ACHIEVED", f"*** {search_name} already completed! ***")
+        else:
+            messagebox.showerror("MISSION START", f">> Time to take {search_name} for a ride! <<")
+    elif add_name:
         if add_name not in champs:
             champs.append(add_name)
             write_champs(champs)
@@ -39,10 +45,13 @@ def handle_entry():
                 write_champs(champs)
                 messagebox.showinfo("VIEGO R", f"-(( {replace_old} transformed into {replace_new} ))-")
             else:
-                messagebox.showwarning("NUH UH", f":: You think this is One for All? ::")
+                messagebox.showwarning("NUH UH", f":: You think this is One for All? {replace_new} is already in the list ::")
         else:
             messagebox.showwarning("MISINPUT", f"-{{ {replace_old} is hiding elsewhere }}-")
-    
+    elif not replace_old and replace_new:
+        messagebox.showwarning("U LOSING IT", "/\\_ Gonna need to know what you're trying to replace bud _/\\")
+    elif replace_old and not replace_new:
+        messagebox.showwarning("U LOSING IT", f"-. What do you want me to replace {replace_old} with? Air?! .-")
     elif remove_name:
         if remove_name in champs:
             champs.remove(remove_name)
@@ -50,14 +59,13 @@ def handle_entry():
             messagebox.showinfo("WHAT", f"?? {remove_name} was fake ??")
         else:
             messagebox.showwarning("NO CAN DO", f"... {remove_name} isn't here ...")
-    
     else:
         messagebox.showwarning("U LOSING IT", "\\_ Might wanna enter something? _/")
-    
     clear_entries()
 
 
 def clear_entries():
+    search_entry.delete(0, tk.END)
     add_entry.delete(0, tk.END)
     replace_entry.delete(0, tk.END)
     to_entry.delete(0, tk.END)
@@ -73,7 +81,7 @@ def update_text_display():
     text_box.tag_add('center', '1.0', 'end')
     text_box.config(state='normal')
     text_box.config(height=40)
-    text_box.grid(row=0, column=2, rowspan=10, sticky='nsew', pady=(5, 5), padx=(15, 0))
+    text_box.grid(row=0, column=2, rowspan=11, sticky='nsew', pady=(5, 5), padx=(15, 0))
     text_box.config(state='disabled')
 
 def on_press(event):
@@ -95,11 +103,11 @@ root.overrideredirect(True)
 root.configure(bg='black', bd=5, relief='solid', highlightbackground='#424242', highlightthickness=3)
 root.attributes('-topmost', True)
 
-window_width = 350
-window_height = 360
+window_width = 390
+window_height = 390
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
-x = (screen_width - window_width) // 10
+x = (screen_width - window_width) // 10*9
 y = (screen_height - window_height) // 2
 root.geometry(f"{window_width}x{window_height}+{x}+{y}")
 
@@ -113,28 +121,30 @@ frame = tk.Frame(root, bg='#000000')
 frame.pack(fill='both', expand=True, padx=20, pady=20)
 
 # column 0 / setting each manually easier than loop for handle_entry
+search_entry = tk.Entry(frame, width=13, font=('Consolas', 12), bg='black', fg='white', insertbackground='white', highlightthickness=1, highlightbackground='#424242', highlightcolor='white')
 add_entry = tk.Entry(frame, width=13, font=('Consolas', 12), bg='black',fg='white', insertbackground='white', highlightthickness=1, highlightbackground='#424242', highlightcolor='white')
 replace_entry = tk.Entry(frame, width=13, font=('Consolas', 12), bg='black', fg='white', insertbackground='white', highlightthickness=1, highlightbackground='#424242', highlightcolor='white')
 to_entry = tk.Entry(frame, width=13, font=('Consolas', 12), bg='black', fg='white', insertbackground='white', highlightthickness=1, highlightbackground='#424242', highlightcolor='white')
 remove_entry = tk.Entry(frame, width=13, font=('Consolas', 12), bg='black', fg='white', insertbackground='white', highlightthickness=1, highlightbackground='#424242', highlightcolor='white')
 
-tk.Label(frame, text="ADD", font=('Consolas', 12), bg='black', fg='white').grid(row=0, column=0)
-tk.Label(frame, text="REPLACE", font=('Consolas', 12), bg='black', fg='white').grid(row=2, column=0)
-tk.Label(frame, text="WITH", font=('Consolas', 12), bg='black', fg='white').grid(row=4, column=0)
-tk.Label(frame, text="REMOVE", font=('Consolas', 12), bg='black', fg='white').grid(row=6, column=0)
+tk.Label(frame, text="SEARCH", font=('Consolas', 12), bg='black', fg='white').grid(row=0, column=0)
+tk.Label(frame, text="ADD", font=('Consolas', 12), bg='black', fg='white').grid(row=2, column=0)
+tk.Label(frame, text="REPLACE", font=('Consolas', 12), bg='black', fg='white').grid(row=4, column=0)
+tk.Label(frame, text="REMOVE", font=('Consolas', 12), bg='black', fg='white').grid(row=7, column=0)
 
-add_entry.grid(row=1, column=0, pady=(0,25))
-replace_entry.grid(row=3, column=0, pady=(0, 5))
-to_entry.grid(row=5, column=0, pady=(0, 25))
-remove_entry.grid(row=7, column=0, pady=(0, 25))
+search_entry.grid(row=1, column=0, pady=(0, 18))
+add_entry.grid(row=3, column=0, pady=(0,18))
+replace_entry.grid(row=5, column=0, pady=(0, 5))
+to_entry.grid(row=6, column=0, pady=(0, 18))
+remove_entry.grid(row=9, column=0, pady=(0, 18))
 
 # column 1
 divider = tk.Frame(frame, width=2, bg='#888888', height=700)
-divider.grid(row=0, column=1, rowspan=10, padx=(25,10), sticky='ns')
+divider.grid(row=0, column=1, rowspan=11, padx=(25,10), sticky='ns')
 
 # column 2
 frame.grid_columnconfigure(2, weight=1)
-frame.grid_rowconfigure(9, weight=1)
+frame.grid_rowconfigure(10, weight=1)
 
 update_text_display()
 
